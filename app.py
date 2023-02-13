7af4e909673d31de3926fa6c289a17f4b068e0b8
Gil Sousa <tiagosousafl@gmail.com>
2023-02-13 08:05:21 +0000

    OpenTelemetry exporting to console

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
+
+provider = TracerProvider()
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello World'
+    with tracer.start_as_current_span("server_request"):
+        return 'Hello World'
 
 app.run(host='0.0.0.0')
